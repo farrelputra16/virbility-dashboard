5148a7df6a7ef746fd5328cf4cfa736cf67ab1e0
farrelputra16 <farrelptrra16@students.unnes.ac.id>
2024-08-06 21:56:34 +0700

    edit blog dan artikel

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -4,8 +4,8 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ route('dashboard') }}">
-            <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="...">
-            <span class="ms-3 font-weight-bold">Soft UI Dashboard Laravel Livewire</span>
+            <img src="../assets/img/logo-virbility.png" class="navbar-brand-img h-250" alt="...">
+            <span class="ms-3 font-weight-bold">Virbility Dashboard</span>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
@@ -114,7 +114,7 @@
                             </g>
                         </svg>
                     </div>
-                    <span class="nav-link-text ms-1">Tables</span>
+                    <span class="nav-link-text ms-1">Blogs and Article</span>
                 </a>
             </li>
             <li class="nav-item">
